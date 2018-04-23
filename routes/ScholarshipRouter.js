a7d8e912a6c5d858f666065e8b5a6e2f887f2434
gabrielchase <gchase.patron@gmail.com>
2018-04-23 17:10:49 +0800

    Add logic to root scholarship route and create post endpoint

--- routes/ScholarshipRouter.js
+++ routes/ScholarshipRouter.js
@@ -1,13 +1,32 @@
 const express = require('express')
 const app = express()
+
 const ScholarshipRouter = express.Router()
+const Scholarship = require('../models/Scholarship.model')
 
 ScholarshipRouter.route('/').get(function(req, res) {
-    res.render('index')
+    Scholarship.find(function(err, scholarships) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('index', {scholarships: scholarships})
+        }
+    })
 })
 
 ScholarshipRouter.route('/create').get(function(req, res) {
     res.render('create_scholarships')
+})
+
+ScholarshipRouter.route('/post').post(function(req, res) {
+    const scholarship = new Scholarship(req.body)
+    scholarship.save()
+        .then(scholarship => {
+            res.redirect('/scholarships')        
+        })
+        .catch(err => {
+            res.status(400).send('Unable to save to the database')
+        })
 })
 
 module.exports = ScholarshipRouter
