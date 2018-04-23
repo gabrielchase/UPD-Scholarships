3f98092cf098a82025f4d3145bc34347e6277ec0
gabrielchase <gchase.patron@gmail.com>
2018-04-24 00:33:18 +0800

    Add to Scholarship model and form

--- models/Scholarship.model.js
+++ models/Scholarship.model.js
@@ -4,6 +4,36 @@ const Schema = mongoose.Schema
 const Scholarship = new Schema({
     name: {
         type: String
+    },
+    minimum_gwa: {
+        type: Number
+    },
+    minimum_units_required: {
+        type: Number
+    },
+    allowed_bad_grades: {
+        4: {
+            type: Boolean,
+            default: true
+        },
+        5: {
+            type: Boolean,
+            default: true
+        },
+        drop: {
+            type: Boolean,
+            default: true
+        },
+        inc: {
+            type: Boolean,
+            default: true
+        }
+    },
+    total_receivable: {
+        type: Number
+    },
+    additional_information: {
+        type: String
     }
 },{
     collection: 'scholarships'
