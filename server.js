063145287ee2860b171276709632481fe3f52309
gabrielchase <gchase.patron@gmail.com>
2018-04-23 16:17:47 +0800

    Require mongoose and body parser

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/updscholarships_db')
+
 const app = express()
-const port = 3000
+const PORT = 3000
 
 const ScholarshipRouter = require('./routes/ScholarshipRouter')
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
 app.use('/scholarships', ScholarshipRouter)
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,'public', 'index.html'));
  })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log('updscholarships server running')
 })
