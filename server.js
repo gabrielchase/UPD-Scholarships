50f09bc62e69a8bc038285231e1711c78f3dea4a
gabrielchase <gchase.patron@gmail.com>
2018-04-28 00:40:45 +0800

    Get Port from process environment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/updscholarships_db'
 
 mongoose.Promise = global.Promise
